eb5605b9093dfd0199ca2e61afbe2b325c7eba1b
nestier <fernan.nestier@gmail.com>
2016-02-09 15:48:32 -0300

    add test to question

--- polls/models.py
+++ polls/models.py
@@ -13,8 +13,13 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
     was_published_recently.boolean = True
+
+    def have_choice(self):
+        return self.choice_set.values()
+    have_choice.boolean = True
 
 
 class Choice(models.Model):
